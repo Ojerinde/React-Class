b9b58d67fb81289faaba7509c66cf9e7aae58560
Joel Ojerinde <joelojerinde@gmail.com>
2022-10-24 15:59:46 +0100

    Edit the custom hook

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -36,18 +36,14 @@ const useFetch = () => {
 
   // The function to send the fetch request. is it expecting a configuration object as well as a function that will get the request transformed data
   const fetchRequest = useCallback(
-    async (requestConfig, getData = () => {}) => {
+    async (configObj, getData = () => {}) => {
       dispatchFn({ type: "LOADING", value: true });
       dispatchFn({ type: "ERROR", value: { hasError: false, message: "" } });
       try {
-        const response = await fetch(requestConfig.url, {
-          method: requestConfig.method ? requestConfig.method : "GET",
-          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
-          headers: requestConfig.headers ? requestConfig.headers : {},
-        });
+        const response = await fetch(configObj.url)
 
         if (!response.ok) {
-          throw new Error(`${requestConfig.errorMessage}`);
+          throw new Error(`Something went wrong!`);
         }
 
         // Retrieving the body data
@@ -59,18 +55,9 @@ const useFetch = () => {
           transformedData.push({
             id: student.id.value,
             name: `${student.name.first} ${student.name.last}`,
-            location: `${student.location.street.name} ${student.location.street.number}`,
             email: student.email,
             img: student.picture.medium,
-            title: student.name.title,
-            username: student.login.username,
-            phone: student.phone,
-            age: student.dob.age,
-            date: student.dob.date,
-            address: `${student.location.street.name} ${student.location.street.number}`,
-            country: student.location.country,
-            city: student.location.city,
-            state: student.location.state,
+    
           });
         }
         // Sending the data to the component that send the request
@@ -78,7 +65,7 @@ const useFetch = () => {
       } catch (err) {
         dispatchFn({
           type: "ERROR",
-          value: { hasError: true, message: requestConfig.errorMessage },
+          value: { hasError: true, message: err.message },
         });
       }
       dispatchFn({ type: "LOADING", value: false });
